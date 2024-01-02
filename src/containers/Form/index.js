516099827bf67e31e447b9df3d01877cb2f08b5e
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-01-02 16:19:32 +0100

    "fix errors form with "message envoyé"

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,7 +16,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
-        onSuccess()
+        onSuccess(true)
       } catch (err) {
         setSending(false);
         onError(err);
