c6c3768ebeeca7accc3d5f2d069aaf342c17edd1
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-01-12 15:32:11 +0100

    put in descending order slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,7 +8,7 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = Array.isArray(data?.focus)
-  ? [...data.focus].sort((evtA, evtB) => (new Date(evtA.date) < new Date(evtB.date) ? -1 : 1))
+  ? [...data.focus].sort((evtA, evtB) => (new Date(evtA.date) > new Date(evtB.date) ? -1 : 1))
   : [];
 
   const totalSlides = byDateDesc.length;
